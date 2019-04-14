f247abdb183477a8a6190073c07cc57833fa42ed
Minoru Miyata <miyarappo@gmail.com>
2019-04-14 18:37:05 +0900

    install css-loader & style-loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import _ from 'lodash'
-import { Nijou as test } from './util'
-
-console.log(test(1000))
+import './style.css'
 
 function component() {
   const element = document.createElement('div');
@@ -11,3 +9,4 @@ function component() {
 }
 
 document.body.appendChild(component());
+document.body.classList.add('haikei');
